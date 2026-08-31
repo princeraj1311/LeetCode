b31fa59a721beef65b484fdc90bd1f8c9be42ce5
Prince Raj <1106pr@gmail.com>
2026-08-31 16:54:46 +0530

    Time: 7 ms (22.03%) | Memory: 46.1 MB (28.27%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0 ; i < (n-2) ; i+=3){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+                
+            }
+        }
+        return nums[n-1];    
+    }
+}
